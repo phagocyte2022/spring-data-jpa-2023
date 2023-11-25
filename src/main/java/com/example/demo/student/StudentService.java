7a1ad1da7feabf74f35f582243f01db15381abca
phagocyte2022 <mp.forcode@gmail.com>
2023-11-25 19:34:23 +0200

    11. DAO layer. POST methgod implementation.

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -22,4 +22,8 @@ public class StudentService {
         return studentRepository.findAll();
     }
 
+
+    public void addNewStudent(Student student) {
+        System.out.println(student);
+    }
 }
