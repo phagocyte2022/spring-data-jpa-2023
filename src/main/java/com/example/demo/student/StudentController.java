ecc81713261d8d6e98a43807b2d2bb02aac321de
phagocyte2022 <mp.forcode@gmail.com>
2023-11-25 16:11:50 +0200

    4. simple business layer (service added)

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -11,14 +11,16 @@ import java.util.List;
 @RestController
 @RequestMapping(path="api/v1/student")
 public class StudentController {
+
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
     @GetMapping
     public List<Student> getStudents(){
-        return List .of(new Student
-                (1L,
-                        "Jane",
-                        "jane.doe@gmail.com",
-                        LocalDate.of(2000, 10, 17),
-                        21));
+        return studentService.getStudents();
     }
 
 }
---
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -0,0 +1,20 @@
+package com.example.demo.student;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class StudentService {
+    @GetMapping
+    public List<Student> getStudents(){
+        return List .of(new Student
+                (1L,
+                        "Jane",
+                        "jane.doe@gmail.com",
+                        LocalDate.of(2000, 10, 17),
+                        21));
+    }
+
+}
