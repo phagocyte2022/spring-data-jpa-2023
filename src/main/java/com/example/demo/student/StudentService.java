bdae1d40a8294a370911ad7b556d959d35fa8ff9
phagocyte2022 <mp.forcode@gmail.com>
2023-11-25 16:18:07 +0200

    5. annotations

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,11 +1,13 @@
 package com.example.demo.student;
 
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@Service
 public class StudentService {
     @GetMapping
     public List<Student> getStudents(){
